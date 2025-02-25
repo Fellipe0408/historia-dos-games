17ad13d39ef02ad2353f6c2899a38e9335829a97
Fellipe Gomes <52552307+Fellipe0408@users.noreply.github.com>
2025-02-25 13:26:04 -0300

    Add files via upload

---
+++ arquivos.js/index.js
@@ -0,0 +1,18 @@
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const dropdown = document.querySelector(".dropdown span");
+        const submenu = document.querySelector(".submenu");
+
+        dropdown.addEventListener("click", function (event) {
+            event.stopPropagation(); // Evita que o clique feche imediatamente
+            submenu.style.display = submenu.style.display === "block" ? "none" : "block";
+        });
+
+        // Fecha o menu ao clicar fora dele
+        document.addEventListener("click", function (event) {
+            if (!dropdown.parentElement.contains(event.target)) {
+                submenu.style.display = "none";
+            }
+        });
+    });
+
